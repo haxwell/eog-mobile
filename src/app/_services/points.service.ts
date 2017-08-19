3b1160e05df275a42332d1979482318c56775996
Johnathan James <haxwell@gmail.com>
2017-08-19 10:41:00 -0600

    Added getCurrentAvailableUserPoints() in place of getUserHasSufficientPointsGivenRules(), due to the new way points are handled in regard to Things (a point can no longer mean a recommendation, so rules are simpler, they are actually recommendations now)

--- src/app/_services/points.service.ts
+++ src/app/_services/points.service.ts
@@ -6,9 +6,25 @@ import { ApiService } from './api.service';
 import { environment } from '../../_environments/environment';
 
 @Injectable()
-export class PointsService { // TODO: should this be named RulesService??
+export class PointsService { 
 	
 	constructor(private _apiService: ApiService, private _userService: UserService) { }
+
+	getCurrentAvailableUserPoints() {
+		return new Promise((resolve, reject) => {
+			let user = this._userService.getCurrentUser();
+			let url = environment.apiUrl + "/api/user/" + user["id"] + "/points";
+			this._apiService.get(url)
+			.subscribe((obj) => {
+				let rtn = JSON.parse(obj["_body"]);
+
+				let sum = 0;
+				rtn.map((obj) => { if (obj["escrowedRequestId"] === null) sum += obj["quantity"]; });
+
+				resolve(sum);
+			});	
+		});
+	}
 
 	getCurrentUserPoints() {
 		return new Promise((resolve, reject) => {
@@ -22,25 +38,7 @@ export class PointsService { // TODO: should this be named RulesService??
 		});
 	}
 
-	// TODO: The client should cache these results, since this method can be repeatedly called by Angular.
-	//  The arrays won't change during the life of the page, so no need to reprocess them over and over.
-	getUserHasSufficientPointsGivenRules(rulesArray, userPoints) {
-		let arr = rulesArray.slice();
-		let upArr = userPoints.slice();
-		
-		for (let x=0; x < arr.length; x++) {
-			let reqdId = arr[x]["requiredUserId"];
-
-			for (let y=0; y < upArr.length; y++) {
-				if (upArr[y]["providingUserId"] === reqdId) {
-					arr[x]["quantity"] -= upArr[y]["quantity"];
-					upArr[y]["used"] = true;
-				}
-			}
-
-			upArr = upArr.filter((obj) => { return obj["used"]; });
-		}
-
-		return arr.every((obj) => { return obj["quantity"] <= 0; });
+	getUserHasSufficientPointsGivenRules(thing, userPoints) {
+		return (thing["requiredPointsQuantity"] <= userPoints);
 	}
 }
