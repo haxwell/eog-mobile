413f55e2f052c1d8512fd20f161abe8e68151f28
Johnathan James <haxwell@gmail.com>
2017-08-20 10:42:00 -0600

    Initial commit

---
+++ src/pages/requests/incoming/_pages/cancel.request.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams, ViewController } from 'ionic-angular';
+
+import { RequestsService } 	from '../../../../app/_services/requests.service';
+
+@Component({
+  selector: 'page-requests-incoming-cancel',
+  templateUrl: 'cancel.request.html'
+})
+
+export class CancelRequestPage {
+
+	confirmationString = '';
+	request = undefined;
+	
+	constructor(public navCtrl: NavController, 
+				public params: NavParams,
+				private viewCtrl: ViewController, 
+				private _requestsService: RequestsService) {
+		this.request = params.get('request');
+	}
+
+	isSaveBtnEnabled() {
+		return this.confirmationString.toLowerCase() === 'cancel';
+	}
+
+	onSaveBtnTap(evt) {
+		this._requestsService.cancelIncomingRequest(this.request).then((obj) => {
+			console.log(obj);
+			this.viewCtrl.dismiss();			
+		})
+	}
+
+	onCancelBtnTap(evt) {
+		this.viewCtrl.dismiss();
+	}
+}
