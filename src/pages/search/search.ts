513d178a696ca913e2b5a6dc9e7311e764959611
Johnathan James <haxwell@gmail.com>
2017-08-22 20:46:13 -0600

    Made better use of promises, so as to disable the Request button when isRequestable() is truly no longer true.

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -20,6 +20,8 @@ export class SearchPage {
 	requestsList = undefined;
 	isRequestableObj = {};
 
+	requestsPromise = undefined;
+
 	constructor(public navCtrl: NavController, 
 				private _searchService: SearchService,
 				private modalCtrl: ModalController,
@@ -30,34 +32,45 @@ export class SearchPage {
 	}
 
 	ngOnInit() {
-		this._recommendationService.init();
+		let self = this;
+		this.isRequestableObj = {};
+		this.userPoints = undefined;
+		this.requestsList = undefined;
+		this._recommendationService.init().then((data) => {
+			self._pointsService.getCurrentAvailableUserPoints().then((data) => {
+				self.userPoints = data;
+			});
 
-		this._pointsService.getCurrentAvailableUserPoints().then((data) => {
-			this.userPoints = data;
-		});
-
-		this._requestsService.getModelForOutgoing().then((data) => {
-			this.requestsList = data;
+			self.requestsPromise = new Promise((resp, rej) => {
+			 	self._requestsService.getModelForOutgoing().then((data) => {
+					resp(data);
+				})
+			})
 		})
 	}
 
 	onSearchBtnTap(evt) {
+		let self = this;
 		this._searchService.searchThings(this.searchString).then((data) => {
-			this.resultList = data;
+			self.resultList = data;
 		});
 	}
 
 	getUserHasSufficientPointsGivenRules(thing) {
-		this.isRequestableObj[thing["id"]]["sufficientPoints"] = this._pointsService.getUserHasSufficientPointsGivenRules(thing, this.userPoints);
+		this.isRequestableObj[thing["id"]]["sufficientPoints"] = thing["requiredPointsQuantity"] <= this.userPoints;
 	}
 
 	getUserHasAlreadyRequestedThisThing(thing) {
-		this.isRequestableObj[thing["id"]]["alreadyRequested"] = this.requestsList.some((obj) => { return obj["thing"]["id"] === thing["id"]; })
+		let self = this;
+		this.requestsPromise.then((data) => {
+			self.isRequestableObj[thing["id"]]["alreadyRequested"] = data.some((obj) => { return obj["thing"]["id"] === thing["id"]; })
+		});
 	}
 
 	getUserHasNecessaryRecommendations(thing) {
+		let self = this;
 		this._recommendationService.getUserHasNecessaryRecommendations(thing).then((data) => {
-			this.isRequestableObj[thing["id"]]["necessaryRecommendations"] = data;
+			self.isRequestableObj[thing["id"]]["necessaryRecommendations"] = data;
 		});
 	}
 
@@ -78,7 +91,7 @@ export class SearchPage {
 	onRequestBtnTap(evt, item) {
 		this.isRequestableObj[item["id"]] = undefined;
 		let modal = this.modalCtrl.create(RequestPage, {thing: item});
-		//modal.onDidDismiss(data => {  });
+		modal.onDidDismiss(data => { this.ngOnInit(); });
 		modal.present();
 	}
 }
