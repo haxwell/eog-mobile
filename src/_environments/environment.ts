2fead95a3b10399621faade31066b92ec35ff918
Johnathan James <haxwell@gmail.com>
2017-08-31 15:36:34 -0600

    broke out the attributes of the apiUrl into smaller reusable pieces.

--- src/_environments/environment.ts
+++ src/_environments/environment.ts
@@ -3,7 +3,15 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+export const domainInfo = {
+  domain: 'localhost',
+  port: '8080'
+};
+
+export const domainPort = domainInfo.domain + ':' + domainInfo.port;
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  domainPort: domainPort,
+  apiUrl: 'http://' + domainPort
 };
