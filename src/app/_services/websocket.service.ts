039b1becad1547ea21504cd0b1c0506f2e7a4156
Johnathan James <haxwell@gmail.com>
2017-09-01 13:08:43 -0600

    Added code to show a Toast component when a new message comes in, and added a method to create messages based on the state of the request sent from the API NotificationService.

--- src/app/_services/websocket.service.ts
+++ src/app/_services/websocket.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 
-import { UserService } from '../../app/_services/user.service';
-import { ApiService } from '../../app/_services/api.service';
+import { ToastController } from 'ionic-angular';
 
+import { UserService } from '../../app/_services/user.service';
+
+import { Constants } from '../../_constants/constants';
 import { environment } from '../../_environments/environment';
 
 import Stomp from 'stompjs';
@@ -12,8 +14,9 @@ export class WebsocketService {
 
 	client = undefined;
 
-	constructor(private _apiService: ApiService, 
-				private _userService: UserService) { 
+	constructor(private _userService: UserService,
+				private toastCtrl: ToastController,
+				private _constants: Constants) { 
 
 	}
 
@@ -27,6 +30,13 @@ export class WebsocketService {
 			console.log("STOMP client connected.");
 			self.client.subscribe("/user/queue/message", (data) => {
 				console.log(data);
+
+				let toast = this.toastCtrl.create({
+					message: self.getMessage(data),
+					duration: 4000,
+					position: 'top'
+				});
+				toast.present();
 			});
 		});
 	}
@@ -35,4 +45,36 @@ export class WebsocketService {
 		return this.client !== undefined && this.client["connected"] === true;
 	}
 
+	getMessage(data) {
+		let obj = JSON.parse(data["body"]);
+		
+		if (obj["request"] !== undefined) {
+			let request = obj["request"];
+			let dou_realname = obj["directionallyOppositeUser"]["realname"];
+
+			if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_PENDING && request["requestingStatusId"] === null) {
+				return dou_realname + ' just requested you do your Thing, ' + obj["request"]["thing"]["title"];
+			} else if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_ACCEPTED && request["requestingStatusId"] === null) {
+				return dou_realname + ' just accepted your request, ' + obj["request"]["thing"]["title"];
+			} else if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_DECLINED && request["requestingStatusId"] === null) {
+				return dou_realname + ' declined your request, ' + obj["request"]["thing"]["title"];
+			} else if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_COMPLETED && request["requestingStatusId"] === null) {
+				return dou_realname + ' just marked your request, ' + obj["request"]["thing"]["title"] + ' as completed.';
+			} else if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_CANCELLED && request["requestingStatusId"] === null) {
+				return dou_realname + ' just cancelled your request, ' + obj["request"]["thing"]["title"];
+			} else if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_COMPLETED && request["requestingStatusId"] === this._constants.REQUEST_STATUS_COMPLETED) {
+				return 'You just received ' + request["thing"]["requiredPointsQuantity"] + ' points from ' + dou_realname;
+			} else if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_COMPLETED && request["requestingStatusId"] === this._constants.REQUEST_STATUS_NOT_COMPLETED) {
+				return dou_realname + ' disagrees that your Thing, ' + obj["request"]["thing"]["title"] + ' was completed. Get in touch with them!';
+			} else if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_DELETED) {
+				return dou_realname + ' deleted their thing, ' + obj["request"]["thing"]["title"] + ', so your request was removed, too.';
+			}
+		} else if (obj["recommendationReceived"] !== undefined) {
+			return "You just received a recommendation";
+		} else if (obj["pointReceived"] !== undefined) {
+			return "You just received a point!"
+		}
+
+		return 'Something interesting just happened!';
+	}
 }
