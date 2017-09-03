9345c393f0d924acfdd20f1b7d95adbd8f91c987
Johnathan James <haxwell@gmail.com>
2017-09-03 09:47:02 -0600

    Added code to reinitialize the websocket connection if a new user logs in. Also, event handling is in place for Request Received and Accepted events. The UI will update when a Request is Accepted.

--- src/app/_services/websocket.service.ts
+++ src/app/_services/websocket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { ToastController } from 'ionic-angular';
+import { Events } from 'ionic-angular';
 
 import { UserService } from '../../app/_services/user.service';
 
@@ -16,33 +17,47 @@ export class WebsocketService {
 
 	constructor(private _userService: UserService,
 				private toastCtrl: ToastController,
-				private _constants: Constants) { 
+				private _constants: Constants,
+				public _events: Events) { 
 
 	}
 
 	init() {
 		let user = this._userService.getCurrentUser();
 		let self = this;
-		if (self.client === undefined) 
-			self.client = Stomp.client('ws://' + environment.domainPort + '/notifications');
+		if (self.client !== undefined) {
+			self.client.disconnect(() => { self.client = undefined; });
+		}
+
+		self.client = Stomp.client('ws://' + environment.domainPort + '/notifications');
 		
 		self.client.connect(user["name"], user["password"], () => { 
 			console.log("STOMP client connected.");
 			self.client.subscribe("/user/queue/message", (data) => {
 				console.log(data);
-
-				let toast = this.toastCtrl.create({
-					message: self.getMessage(data),
-					duration: 4000,
-					position: 'top'
-				});
-				toast.present();
+				self.handle(JSON.parse(data["body"]));
 			});
 		});
 	}
 
 	isInitialized() {
 		return this.client !== undefined && this.client["connected"] === true;
+	}
+
+	handle(data) {
+		if (data["request"] !== undefined) {
+			data["request"]["directionallyOppositeUser"] = Object.assign({}, data["directionallyOppositeUser"]);
+			delete data["directionallyOppositeUser"];
+
+			let request = data["request"];
+			let dou_realname = request["directionallyOppositeUser"]["realname"];
+
+			if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_PENDING && request["requestingStatusId"] === null) {
+				this.handleRequestReceived(data)
+			} else if (request["deliveringStatusId"] === this._constants.REQUEST_STATUS_ACCEPTED && request["requestingStatusId"] === null) {
+				this.handleRequestAccepted(data);
+			} 
+		}
 	}
 
 	getMessage(data) {
@@ -76,5 +91,30 @@ export class WebsocketService {
 		}
 
 		return 'Something interesting just happened!';
+	}
+
+	presentToast(msg) {
+		let toast = this.toastCtrl.create({
+			message: msg,
+			duration: 4000,
+			position: 'top'
+		});
+		toast.present();
+	}
+
+	handleRequestReceived(data) {
+		let request = data["request"];
+		let dou_realname = request["directionallyOppositeUser"]["realname"];
+
+		this.presentToast(dou_realname + ' just requested you do your Thing, ' + request["thing"]["title"]);
+		this._events.publish('request:received', data);
+	}
+
+	handleRequestAccepted(data) {
+		let request = data["request"];
+		let dou_realname = request["directionallyOppositeUser"]["realname"];
+
+		this.presentToast(dou_realname + ' just accepted your request, ' + request["thing"]["title"]);
+		this._events.publish('request:accepted', data);
 	}
 }
