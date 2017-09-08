51c279b6e550a528dc1e1833fdb8c60e412e65b2
Johnathan James <haxwell@gmail.com>
2017-09-08 08:07:12 -0600

    minor, formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,18 @@ import { LoginPage } from '../pages/login/login';
 export class MyApp {
   rootPage:any = LoginPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(private platform: Platform, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen) {
+    
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  
   }
+
 }
 
