deb7c36166ad9918d33e5fff6bb6bae1c2ba2495
Johnathan James <haxwell@gmail.com>
2017-09-21 08:19:36 -0600

    First Unit Test now running: KeywordEntryPage

---
+++ src/pages/keyword.entry/keyword.entry.spec.ts
@@ -0,0 +1,50 @@
+import { async, TestBed } from '@angular/core/testing';
+import { IonicModule, Platform } from 'ionic-angular';
+import { ViewController } from 'ionic-angular';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { KeywordEntryPage } from './keyword.entry';
+import {
+  PlatformMock,
+  SplashScreenMock,
+  ViewControllerMock
+} from '../../../test-config/mocks-ionic';
+
+import { StatusBarMock } from 'ionic-mocks'
+
+describe('KeywordEntryPage Component', () => {
+  let fixture;
+  let component;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [KeywordEntryPage],
+      imports: [
+        IonicModule.forRoot(KeywordEntryPage)
+      ],
+      providers: [
+        { provide: StatusBar, useClass: StatusBarMock },
+        { provide: SplashScreen, useClass: SplashScreenMock },
+        { provide: Platform, useClass: PlatformMock },
+        { provide: ViewController, useClass: ViewControllerMock }
+      ]
+    })
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KeywordEntryPage);
+    component = fixture.componentInstance;
+  });
+
+  it('should be created', () => {
+    expect(component instanceof KeywordEntryPage).toBe(true);
+  });
+
+  it('should not have save button enabled', () => {
+    expect(component.keywordString).toBe(undefined);
+    expect(component.isSaveBtnEnabled()).toBe(false);
+  });
+
+});
