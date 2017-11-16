c5d7691c31275bce423751099140e9d0c5b57525
Johnathan James <haxwell@gmail.com>
2017-11-16 16:10:35 -0700

    #35, Press the back button twice at the Home page to log out. Press it twice again to exit the app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
-import { Platform, Events } from 'ionic-angular';
+import { Platform, Nav, Events } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { LoginPage } from '../pages/login/login';
@@ -10,14 +10,38 @@ import { LoginPage } from '../pages/login/login';
 })
 export class EasyahApp {
   rootPage:any = LoginPage;
+  lastTimeBackPress = 0;
+  timePeriodToExit = 750;
+
+  @ViewChild(Nav) navCtrl: Nav;
 
   constructor(platform: Platform,
-              _events: Events, 
               statusBar: StatusBar) {
 
+    let self = this;
     platform.registerBackButtonAction(() => {
-        _events.publish("hardwareBackButtonPressed", { });
-    });
+          // get current active page
+          let view = self.navCtrl.getActive();
+
+          if (view.component.name == "HomePage") {
+              // press Back twice to logout..
+              if (new Date().getTime() - self.lastTimeBackPress < self.timePeriodToExit) {
+                  self.navCtrl.pop();
+              } else {
+                  self.lastTimeBackPress = new Date().getTime();
+              }
+          } else if (view.component.name == "LoginPage") {
+              // press Back twice again to exit the app..
+              if (new Date().getTime() - self.lastTimeBackPress < self.timePeriodToExit) {
+                  platform.exitApp();
+              } else {
+                  self.lastTimeBackPress = new Date().getTime();
+              }
+          } else {
+              // go to previous page
+              self.navCtrl.pop({});
+          }
+      });
     
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
