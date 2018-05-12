77480cb21734a68027b1c363475632e5df6db31c
Johnathan James <haxwell@gmail.com>
2018-05-12 02:13:01 -0600

    Minor.. cleaning up code in profile-header

--- src/pages/common/profile-header/profile-header.ts
+++ src/pages/common/profile-header/profile-header.ts
@@ -4,8 +4,6 @@ import { Events } from 'ionic-angular';
 
 import { File } from '@ionic-native/file'
 
-import { ChoosePhotoSourcePage } from '../choose-photo-source/choose-photo-source'
-
 import { ProfileService } from '../_services/profile.service'
 
 import EXIF from 'exif-js'
@@ -17,23 +15,13 @@ import EXIF from 'exif-js'
 
 export class ProfileHeader {
 
-	@Input() readOnly = false;
 	dirty = false;
 	@Input() user = undefined;
-	modalCtrl = undefined;
 
 	imageOrientation = undefined;
 
-	constructor(/*navParams: NavParams, */
-				modalCtrll: ModalController, 
-				private _profileService: ProfileService, 
-				private _events: Events,
-				private _file: File) {
-
-		this.modalCtrl = modalCtrll;
-
-		//this.user = Object.assign({}, navParams.get('user'));
-		//this.readOnly = navParams.get('readOnly') || false;
+	constructor(private _profileService: ProfileService, 
+				private _events: Events) {
 
 		let self = this;
 		let func2 = (data) => {
@@ -57,14 +45,6 @@ export class ProfileHeader {
 			this.ngOnInit();
 	}
 
-	isCurrentUserAllowedToSeeContactInfo() {
-		return this._profileService.getModel(this.user)["currentUserCanSeeContactInfo"];
-	}
-
-	isReadOnly() {
-		return this.readOnly;
-	}
-
 	isDirty() {
 		return this.dirty;
 	}
@@ -85,85 +65,7 @@ export class ProfileHeader {
 		if (this._profileService.getModel(this.user)["imageFileURI"] === undefined)
 			return "assets/img/mushroom.jpg";
 		else
-
-			// WILO: Is this being called when we go from Profile to Home? If so, is this a different model?
-			//  why is the picture not changing?
-
 			return this._profileService.getModel(this.user)["imageFileURI"];
-	}
-
-	onThumbnailPress($event) {
-		if (!this.isReadOnly()) {
-			let self = this;
-			let model = this._profileService.getModel(this.user);
-			let modal = this.modalCtrl.create(ChoosePhotoSourcePage, {userId: this.user["id"], fileURI: model["imageFileURI"], fileSource: model["imageFileSource"]});
-			
-			modal.onDidDismiss((promise) => {
-				if (promise) {
-					promise.then((uriAndSource) => { 
-						if (uriAndSource === undefined) {
-							uriAndSource = {};
-						}
-
-						let model = this._profileService.getModel(this.user);
-
-						if (model["imageFileURI"] !== undefined && model["imageFileSource"] == 'camera') {
-							let lastSlash = model["imageFileURI"].lastIndexOf('/');
-							let path = model["imageFileURI"].substring(0,lastSlash+1);
-							let filename = model["imageFileURI"].substring(lastSlash+1);
-
-							self._file.removeFile(path, filename).then((data) => {
-								this._profileService.setMostProbableProfilePhotoPath(uriAndSource["imageFileURI"]);
-
-								console.log("User saved a new profile image. [" + model["imageFileURI"] + "] is no longer the image to use, so it has been removed." );
-								console.log("setting profile header model to [" + uriAndSource["imageFileURI"] + "], and throwing profile:changedProfileImage event");
-
-								model["imageFileURI"] = uriAndSource["imageFileURI"];
-								model["imageFileSource"] = uriAndSource["imageFileSource"];
-
-								self._events.publish('profile:changedProfileImage', model["imageFileURI"]);
-								self.setDirty(true);						
-							})
-						} else {
-							console.log("no previous image to delete, so skipping that step...")
-							console.log("setting profile header model to [" + uriAndSource["imageFileURI"] + "], and throwing profile:changedProfileImage event");
-
-							model["imageFileURI"] = uriAndSource["imageFileURI"];
-							model["imageFileSource"] = uriAndSource["imageFileSource"];
-
-							self._events.publish('profile:changedProfileImage', model["imageFileURI"]);
-							self.setDirty(true);
-						}
-
-					});
-				}
-			});
-			
-			modal.present();
-		}
-	}
-
-	setChangedAttr(key, value) {
-		let model = this._profileService.getModel(this.user);
-		model[key] = value;
-		this._events.publish('profile:changedContactInfo', model);
-		this.setDirty(true);
-	}
-
-	onNameChange(event) {
-		this.setChangedAttr("realname", event._value);
-	}
-
-	onDescriptionChange(event) {
-		this.setChangedAttr("description", event._value);
-	}
-
-	onEmailChange(event) {
-		this.setChangedAttr("email", event._value);
-	}
-
-	onPhoneChange(event) {
-		this.setChangedAttr("phone", event._value);
 	}
 
 	getModelAttr(key) {
