f346682dc4f3622b0f3b130b58c9d12166e8ef88
Johnathan James <haxwell@gmail.com>
2018-06-09 10:38:11 -0600

    Cleaning up unused code in profile-header.. specifically, binding the User variable is no longer necessary, because this component is only used for the current user

--- src/pages/common/profile-header/profile-header.ts
+++ src/pages/common/profile-header/profile-header.ts
@@ -1,8 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { ModalController } from 'ionic-angular';
+import { Component } from '@angular/core';
 import { Events } from 'ionic-angular';
-
-import { File } from '@ionic-native/file'
 
 import { ProfileService } from '../_services/profile.service'
 
@@ -15,79 +12,37 @@ import EXIF from 'exif-js'
 
 export class ProfileHeader {
 
-	dirty = false;
-	@Input() user = undefined;
-
+	userId = undefined;
 	imageOrientation = undefined;
 
 	constructor(private _profileService: ProfileService, 
 				private _events: Events) {
 
 		let self = this;
-		let func2 = (data) => {
-			let model = this._profileService.getModel(this.user["id"]);
-
-			model["realname"] = data["realname"];
-			model["phone"] = data["phone"];
-			model["email"] = data["email"];
-
-			self.setDirty(true);
-		};
-		self._events.subscribe('profile:changedContactInfoWasSaved', func2);
-
-		// TODO: This is ugly. We do this because this is part of the app.html file, because
-		//  that is where the menu is defined, and that is called first before anything else.
-		//  so, there's no user at that time. This component is also used when viewing another
-		//  user's profile. So it needs a user objet. So we get one for the current user so this
-		//  can display in the menu properly when the menu comes up, and we set it in the html
-		//  (by passing a function which calls for the user to be displayed) elsewhere.
-
-		// Ideally, this wouldn't even be called until a user was ready. And then we could
-		//  ONLY set it through the HTML, not need this event sub. Regardless, yuk poo.
-		self._events.subscribe('app:login', (currentUser) => { this.user = currentUser; });
+		self._events.subscribe('app:login', (currentUser) => { this.userId = currentUser["id"]; });
 	}
 
 	ngOnInit() {
 
 	}
 
-	ionViewWillEnter() {
-		if (this.isDirty()) 
-			this.ngOnInit();
-	}
-
-	isDirty() {
-		return this.dirty;
-	}
-
-	setDirty(b) {
-		this.dirty = b;
-	}
-
-	isFromGallery() {
-		if (this.user)
-			return this._profileService.getModel(this.user["id"])["imageFileSource"] == 'gallery';
-		else
-			return false;
-	}
-
 	isThumbnailImageAvailable() {
-		if (this.user)
-			return this._profileService.getModel(this.user["id"])["imageFileURI"] !== undefined;
+		if (this.userId)
+			return this._profileService.getModel(this.userId)["imageFileURI"] !== undefined;
 		else
 			return false;
 	}
 
 	getThumbnailImage() {
-		if (this.user && this._profileService.getModel(this.user["id"])["imageFileURI"] !== undefined)
-			return this._profileService.getModel(this.user["id"])["imageFileURI"];
+		if (this.userId && this._profileService.getModel(this.userId)["imageFileURI"] !== undefined)
+			return this._profileService.getModel(this.userId)["imageFileURI"];
 		else
 			return "assets/img/mushroom.jpg";
 	}
 
 	getModelAttr(key) {
-		if (this.user) {
-			let model = this._profileService.getModel(this.user["id"]) || {};
+		if (this.userId) {
+			let model = this._profileService.getModel(this.userId) || {};
 			return model[key];
 		} else {
 			return undefined;
