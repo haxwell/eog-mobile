953ff28c0a6ebe7c86e012539c23b29b251a952f
Johnathan James <haxwell@gmail.com>
2018-06-08 22:27:22 -0600

    Added a production attribute to the environment.* files so that I can tell when we're in production or not.

--- src/_environments/environment.staging.ts
+++ src/_environments/environment.staging.ts
@@ -7,6 +7,7 @@ export const domainInfo = {
 export const domainPort = domainInfo.domain + ':' + domainInfo.port;
 
 export const environment = {
+  production: false,
   domainPort: domainPort,
   apiUrl: 'http://' + domainPort
 };
