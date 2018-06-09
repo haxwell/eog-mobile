eb81a90dc8c28e2e2a123ef5f6c8dc483e99b136
Johnathan James <haxwell@gmail.com>
2018-06-09 14:36:51 -0600

    Added ApiService and ProfilePictureService mocks to mocks-easyah.ts

--- test-config/mocks-easyah.ts
+++ test-config/mocks-easyah.ts
@@ -1,9 +1,14 @@
-//import { StatusBar } from '@ionic-native/status-bar';
 
 export class UserServiceMock {
+	public getCurrentUser(): any { 
+	    return {id:1,name:"eogadmin",realname:"Eog A. Admin",email:"admin@eog.com",phone:"(303) 555-1212",enabled:1};
+	};
+}
 
-public getCurrentUser(): any { 
-    return {id:1,name:"eogadmin",realname:"Eog A. Admin",email:"admin@eog.com",phone:"(303) 555-1212",enabled:1};
-};
+export class ApiServiceMock {
+	
+}
 
+export class ProfilePictureServiceMock {
+	
 }
