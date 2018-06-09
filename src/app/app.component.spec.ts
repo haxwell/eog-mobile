c151e87bd8b3f5a65cc8697d685ee0152f6f16ce
Johnathan James <haxwell@gmail.com>
2018-06-09 14:49:07 -0600

    Added a bunch of service and component definitions to app.component.spec.ts, for unit testing purposes. As in, they weren't running before this.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,11 +5,35 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { EasyahApp } from './app.component';
+import { ProfileHeader } from '../pages/common/profile-header/profile-header';
+import { ProfilePoints } from '../pages/common/profile-points/profile-points';
+
+import { ApiService } from './_services/api.service';
+import { Constants } from '../_constants/constants';
+import { DeclineReasonCodeService } from './_services/declined-reason-codes.service';
+import { NotificationService } from './_services/notification.service';
+import { PointsService } from './_services/points.service';
+import { ProfileService } from '../pages/common/_services/profile.service';
+import { ProfilePictureService } from './_services/profile-picture.service';
+import { RecommendationService } from './_services/recommendation.service';
+import { RequestsService } from './_services/requests.service';
+import { SearchService } from './_services/search.service';
+import { UnseenChangesIndicatorService } from './_services/unseen-changes-indicator.service';
+import { UserService } from './_services/user.service';
+import { UserPreferencesService } from './_services/user-preferences.service';
+import { WebsocketService } from './_services/websocket.service';
+
 import {
   PlatformMock,
   StatusBarMock,
   SplashScreenMock
 } from '../../test-config/mocks-ionic';
+
+import {
+  ApiServiceMock,
+  UserServiceMock,
+  ProfilePictureServiceMock
+} from '../../test-config/mocks-easyah';
 
 describe('EasyahApp Component', () => {
   let fixture;
@@ -17,11 +41,25 @@ describe('EasyahApp Component', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [EasyahApp],
+      declarations: [EasyahApp, ProfileHeader, ProfilePoints],
       imports: [
         IonicModule.forRoot(EasyahApp)
       ],
       providers: [
+        { provide: ApiService, useClass: ApiServiceMock },
+        Constants,
+        DeclineReasonCodeService,
+        PointsService,
+        ProfileService,
+        { provide: ProfilePictureService, useClass: ProfilePictureServiceMock },
+        RecommendationService,
+        RequestsService,
+        SearchService,
+        UnseenChangesIndicatorService,
+        { provide: UserService, useClass: UserServiceMock },
+        UserPreferencesService,
+        WebsocketService,
+
         { provide: StatusBar, useClass: StatusBarMock },
         { provide: SplashScreen, useClass: SplashScreenMock },
         { provide: Platform, useClass: PlatformMock }
