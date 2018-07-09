6c11402383f3376e7a547042433d0da16ed2a347
Johnathan James <haxwell@gmail.com>
2018-07-09 07:05:48 -0600

    EventSubscriberService, removed comments

--- src/app/_services/event-subscriber.service.ts
+++ src/app/_services/event-subscriber.service.ts
@@ -12,22 +12,15 @@ export class EventSubscriberService {
 	}
 
 	subscribe(key, handler) {
-
 		if (!this.handlers[key]) {
 			this.handlers[key] = handler;
 			this._events.subscribe(key, handler)
-			console.log("*1a subscribed to " + key + " event")
-		} else {
-			console.log("** " + key + " has already been subscribed to")
 		}
-
 	}
 
 	reset(key) {
 		this._events.unsubscribe(key, this.handlers[key]);
 		this.handlers[key] = undefined;
-
-		console.log("*1a " + key + " has been UNsubscribed");
 	}
 
 }
